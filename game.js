22b5a554879d104a9d7421c55808f9e0c0c17e9d
shumeikov <viktor.shumeiko@simbirsoft.com>
2025-07-31 18:12:58 +0300

    v1

--- game.js
+++ game.js
@@ -52,6 +52,64 @@ let savedPlayerPos = null;
 let backgroundSound, carSound, trainSound, coinSound, stepSound;
 
 function preload() {
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(40, 270, 220, 50);
+
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var loadingText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 50,
+        text: 'Loading...',
+        style: {
+            font: '20px monospace',
+            fill: '#ffffff'
+        }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 5,
+        text: '0%',
+        style: {
+            font: '18px monospace',
+            fill: '#ffffff'
+        }
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    var assetText = this.make.text({
+        x: width / 2,
+        y: height / 2 + 70,
+        text: '',
+        style: {
+            font: '18px monospace',
+            fill: '#ffffff'
+        }
+    });
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value) {
+        percentText.setText(parseInt(value * 100) + '%');
+        progressBar.clear();
+        progressBar.fillStyle(0xffffff, 1);
+        progressBar.fillRect(40, 270, 300 * value, 30);
+    });
+
+    this.load.on('fileprogress', function (file) {
+        assetText.setText('Loading asset: ' + file.key);
+    });
+
+    this.load.on('complete', function () {
+        progressBar.destroy();
+        progressBox.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+        assetText.destroy();
+    });
     // Анимации игрока
     this.load.spritesheet('idle_down', 'assets/businessman1_idle_down.png', { frameWidth: 8, frameHeight: ROW_HEIGHT_PLAYER });
     this.load.spritesheet('idle_up', 'assets/businessman1_idle_up.png', { frameWidth: 8, frameHeight: ROW_HEIGHT_PLAYER });
